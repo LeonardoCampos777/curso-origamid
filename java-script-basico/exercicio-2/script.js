bea64eab1a3619f92909637c4a5141f8f046d267
Leonardo Campos <leonardo.kiss66@live.com>
2023-06-25 02:24:24 -0300

    update

---
+++ java-script-basico/exercicio-2/script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('nav');
+const produtos = document.querySelector('.produtos');
+const homeLink = document.querySelector('#menu li a');
+
+
+
+
+console.log('nav');
+console.log('produtos');
+console.log('homeLink');
