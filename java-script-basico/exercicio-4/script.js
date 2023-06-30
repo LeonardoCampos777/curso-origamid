2be486e8d79cbab044a64decb5709efbb2572e61
Leonardo Campos <leonardo.kiss66@live.com>
2023-06-29 23:15:58 -0300

    update

---
+++ java-script-basico/exercicio-4/script.js
@@ -0,0 +1,18 @@
+let total = 0;
+
+const compras = 30;
+const imposto = 10;
+
+total = compras + imposto;
+
+
+const dobro = total * 2;
+const metade = total / 2;
+const desconto = total - 10;
+
+
+const strings = '20' + '20';
+const numbers = 20 + 20;
+
+
+console.log(numbers);
